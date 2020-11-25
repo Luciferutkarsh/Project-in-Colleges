5466d8a2570c211e406d315c328029b3032a54ff
LuciferUtkarsh <luciferutkarsh@gmail.com>
2020-11-25 21:22:38 +0530

    Initial Commit

--- ContactsManagement/main.c
+++ ContactsManagement/main.c
@@ -3,7 +3,7 @@
 #include<string.h>
 #include<process.h>
 #include<stdlib.h>
-#include<dos.h>
+#include<direct.h>
 
 
 
@@ -27,7 +27,7 @@ int main()
 main:
 system("cls");    /* ************Main menu ***********************  */
 printf("\n\t **** Welcome to a0 contact Manager ****");
-printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edita Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
+printf("\n\n\n\t\t\tMAIN MENU\n\t\t=====================\n\t\t[1] Add a new Contact\n\t\t[2] List all Contacts\n\t\t[3] Search for contact\n\t\t[4] Edit a Contact\n\t\t[5] Delete a Contact\n\t\t[0] Exit\n\t\t=================\n\t\t");
 printf("Enter the choice:");
 scanf("%d",&ch);
 
